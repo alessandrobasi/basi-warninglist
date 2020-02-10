657343939df43aaa52a46f2c545637c271c2f2e9
alessandrobasi <alessandrobasi@live.it>
2020-02-10 23:32:37 +0100

    update

--- tools/SFP-bad-ips/main.py
+++ tools/SFP-bad-ips/main.py
@@ -1,23 +1,65 @@
 
 import os
+from requests.api import get
+from shutil import copyfileobj
+from zipfile import ZipFile
+from os import listdir, remove
 
 dir_name = os.path.basename(os.path.dirname(os.path.realpath(__file__)))
 save_path = "../../lists/"+dir_name+"/"
 
 def main():
-    
     ips = set()
+    ipv4 = list()
+    ipv6 = list()
 
-    with open("bad1.txt", "r", encoding="UTF-8") as f:
+    url_sfs_30d = "https://www.stopforumspam.com/downloads/listed_ip_30_ipv46.zip"
+    
+    dowload_name = "sfs-bad-ip.zip"
+
+    # Download
+    with get(url_sfs_30d, stream=True) as r:
+        with open(dowload_name, 'wb') as f:
+            copyfileobj(r.raw, f)
+
+    # unzip
+    with ZipFile(dowload_name, 'r') as zipObj:
+        lista_nomi_file = zipObj.namelist()[0]
+        zipObj.extractall()
+    
+    # del zip
+    remove(dowload_name)
+
+    # load IP file
+    with open(save_path+"all.txt","r",encoding="UTF-8") as f:
         for line in f:
             ips.add(line[:-1])
-    with open("bad2.txt", "r", encoding="UTF-8") as f:
+    
+    # Add New IP
+    with open(lista_nomi_file,"r",encoding="UTF-8") as f:
         for line in f:
             ips.add(line[:-1])
-    with open(save_path+"ipv4.txt", "w", encoding="UTF-8") as f:
+
+    # Remove new IP list
+    remove(lista_nomi_file)
+
+    for ip in ips:
+        if '.' in ip:
+            ipv4.append(ip)
+        else:
+            ipv6.append(ip)
+
+    with open(save_path+"ipv4.txt","w", encoding="UTF-8") as f:
+        for ip in ipv4:
+            f.write(ip+"\n")
+    
+    with open(save_path+"ipv6.txt","w", encoding="UTF-8") as f:
+        for ip in ipv6:
+            f.write(ip+"\n")
+    
+    with open(save_path+"all.txt","w", encoding="UTF-8") as f:
         for ip in ips:
             f.write(ip+"\n")
-
 
 
 if __name__ == "__main__":
---
+++ tools/nordvpn-ips/main.py
@@ -0,0 +1,75 @@
+
+import re
+import requests
+import json
+import time
+import os
+from random import randrange
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def getIds(test_str):
+    ids = list()
+    regex = r"data-value=\"(\d+)\""
+    matches = re.finditer(regex, test_str, re.MULTILINE)
+    for _, match in enumerate(matches, start=1):
+        for groupNum in range(1, len(match.groups())+1):
+            ids.append(match.group(groupNum))
+    ids.sort(key=int)
+    return ids
+
+def updateIds():
+
+    driver = webdriver.Firefox(executable_path=r'C:\\Program Files\\Mozilla Firefox\\geckodriver.exe')
+    driver.minimize_window()
+    wait = WebDriverWait(driver,4)
+
+    driver.get('https://nordvpn.com/it/servers/tools/')
+
+    lista_stati = wait.until( EC.presence_of_element_located( (By.XPATH, '//*[@id="recommended"]/div/div/div[2]/div[2]/div/div[1]/div/div[2]/div' ) ) ) 
+    lista_str = lista_stati.get_attribute('innerHTML')
+    driver.quit()
+
+    return getIds(lista_str)
+
+
+dir_name = os.path.basename(os.path.dirname(os.path.realpath(__file__)))
+save_path = "../../lists/"+dir_name+"/"
+
+def main():
+
+    ip = set()
+    with open(save_path+"all.txt","r",encoding="UTF-8") as f:
+        for line in f:
+            ip.add(line[:-1])
+
+    ids = updateIds()
+
+    url_ = 'https://nordvpn.com/wp-admin/admin-ajax.php'
+    headers = {'user-agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36'}
+
+    for num in ids:
+        attesa = randrange(10,60,1)
+        print("attesa di",attesa)
+        time.sleep(attesa)
+        print("scaricando",num)
+        PARAMS = {'action': "servers_recommendations", "filters": '{"country_id": '+ num +'}'}
+        r = requests.get(url =url_, params = PARAMS, headers=headers)
+        data = r.json()
+
+        for i in range(len(data)):
+            ip.add(data[i]["station"])
+
+    with open(save_path+"all.txt","w",encoding="UTF-8") as f:
+        for i in ip:
+            f.write(i+"\n")
+    
+    return "ip trovati unici: "+str(len(ip))
+        
+
+if __name__ == "__main__":
+    print(main())
